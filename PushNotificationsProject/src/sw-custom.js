1d3335c76bc2dd17d35e81c70dcae0a60edb8413
Sajku <kielek0412@gmail.com>
2023-01-31 18:17:02 +0100

    Try no-cors

--- PushNotificationsProject/src/sw-custom.js
+++ PushNotificationsProject/src/sw-custom.js
@@ -1,17 +1,17 @@
-// self.addEventListener('fetch', function (event) {
-//     if (event.request.url.includes('google') && !event.request.url.includes('calendar')) {
-//         doAction(event);
-//     }
-// });
+self.addEventListener('fetch', function (event) {
+    if (event.request.url.includes('google') && !event.request.url.includes('calendar')) {
+        doAction(event);
+    }
+});
 
-// async function doAction(x) {
-//     let headers = new Headers();
-//     headers.append("Content-Security-Policy", "connect-src *;");
-//     headers.append("Access-Control-Allow-Origin", "*");
-//     x.respondWith(
-//         fetch(x.request, { headers: headers, mode: 'no-cors' })
-//     );
-// }
+async function doAction(x) {
+    let headers = new Headers();
+    headers.append("Content-Security-Policy", "connect-src *;");
+    headers.append("Access-Control-Allow-Origin", "*");
+    x.respondWith(
+        fetch(x.request, { headers: headers, mode: 'no-cors' })
+    );
+}
 
 self.addEventListener('notificationclick', function (event) {
     console.log("Notification Clicked");
